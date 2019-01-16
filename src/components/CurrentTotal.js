c968b0e245852fcd2bb6fa4143b553e0f03858e6
Bode Falade <bode.falade@outlook.com>
2019-01-16 16:37:23 -0600

    Up and Down buttons up. INput up.

---
+++ src/components/CurrentTotal.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class CurrentTotal extends Component {
+    
+    render(){
+        const totalList = [];
+        
+
+        return(
+            <div>
+
+
+            </div>
+
+        );
+    }
+
+}
+
+export default CurrentTotal;
--- src/components/EnterNumber.js
+++ src/components/EnterNumber.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 
 class EnterNumber extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            upNumber: 0,
+            downNumber: 0,
+        }
+    }
+
+    getNumber = (event) => {
+        this.setState({
+            inputNumber: event.target.value,
+        });
+    }
+
+    addUp = (event) => {
+        const up = {
+            upNumber: (this.state.upNumber + 1),
+        };
+        this.props.addToCurrentTotal(up);
+    }
+
+    addDown = (event) => {
+        const down = {
+            downNumber: (this.state.downNumber -1),
+        };
+        this.state.addToCurrentTotal(down);
+    }
 
     render() {
         return (
-            <form>
-                <button>Up</button>
-                <input  type="number" placeholder="Enter Number" />
-                <button>Down</button>
-            </form>
+            <div>
+                <button onClick={this.addUp}>Up</button>
+                <input onChange={this.getNumber}  type="number" placeholder="Enter Number" />
+                <button onClick={this.addDown}>Down</button>
+                </div>
         );
     }
 
